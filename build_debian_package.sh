936bd1b867ba3a1de4952f9284e67809c6f8ca05
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-24 08:35:25 +0200

    Use fpm

--- build_debian_package.sh
+++ build_debian_package.sh
@@ -1,22 +1,37 @@
 #!/bin/bash -e
 # bash script to create a simple debian package
 
+gem install fpm
+
 PACKAGE_NAME=$1
 PACKAGE_VERSION=$2
 BUILD_DIR=$3
+BUILD_NUMBER=${CIRCLE_BUILD_NUM}
 
-# create necessary file system structure for package
-mkdir -p $BUILD_DIR/package/$PACKAGE_NAME/DEBIAN
-mkdir -p $BUILD_DIR/package/$PACKAGE_NAME/usr/local/bin
+mkdir temp
+cd temp
+
+# Set fpm arguments
+declare -a FPM_ARGS
+FPM_ARGS+=(-s dir)
+FPM_ARGS+=(-t deb)
+FPM_ARGS+=(--force)
+FPM_ARGS+=(--verbose)
+FPM_ARGS+=(--vendor "Docker")
+FPM_ARGS+=(--category "net")
+FPM_ARGS+=(--maintainer "Hypriot")
+FPM_ARGS+=(--architecture "armhf")
+FPM_ARGS+=(--url "http://blog.hypriot.com")
+FPM_ARGS+=(--license "Apache License Version 2.0")
+FPM_ARGS+=(--name "${PACKAGE_NAME}")
+FPM_ARGS+=(--version "${PACKAGE_VERSION}")
+FPM_ARGS+=(--description "docker-machine for armhf")
+FPM_ARGS+=(--prefix /usr/local/bin)
+FPM_ARGS+=(--iteration ${BUILD_NUMBER})
+FPM_ARGS+=(--package ../)
 
 # copy binary to destination
-cp $BUILD_DIR/machine/bin/docker-machine* $BUILD_DIR/package/$PACKAGE_NAME/usr/local/bin/docker-machine
+cp $BUILD_DIR/machine/bin/docker-machine* docker-machine
 
-# copy package control template and replace version info
-cp -v ./control_file_template $BUILD_DIR/package/$PACKAGE_NAME/DEBIAN/control
-sed -i'' "s/<VERSION>/${PACKAGE_VERSION}/g" $BUILD_DIR/package/$PACKAGE_NAME/DEBIAN/control
-chown -R root:root $BUILD_DIR/package/usr
-
-# actually create package with dpkg-deb
-cd $BUILD_DIR/package
-dpkg-deb --build $PACKAGE_NAME
+set -x
+fpm "${FPM_ARGS[@]}" .
